bc4ec5b6352e28eef4551bc6fa2d9b336c8c8b98
Loi Van Tran <datloiboi@gmail.com>
2021-04-15 10:43:52 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+import "./App.css";
+
+// 0. Use environment variable to make API Request
+
+console.log({ env: process.env });
+
+
 
 function App() {
+  const [weather, setWeather] = useState(null)
+
+  useEffect(() => {
+    const fetchWeather = async (lat, lon) => {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`,
+      );
+      const json = await response.json();
+      setWeather(json);
+    };
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const { latitude, longitude } = pos.coords;
+      fetchWeather(latitude, longitude);
+    });
+  }, []);
+
+  console.log({ weather });
+
+  if (!weather) {
+    return <h1>Loading!</h1>
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather app!</h1>
+      <h3>{weather.name}</h3>
+      <h3>{weather.main.temp}</h3>
     </div>
   );
 }
